445d849124cf7b4fa32d1d371cae88472eb17b3d
cassianogn <33898839+cassianogn@users.noreply.github.com>
2022-08-01 05:18:50 -0300

    configuração inicial

---
+++ clone repositorios.bat
@@ -0,0 +1,8 @@
+cd ../
+git clone https://github.com/desenvspintec/Ioutility.Bus.Commands.git
+git clone https://github.com/desenvspintec/Ioutility.Franquia.Service.git
+git clone https://github.com/desenvspintec/Ioutility.Adm.Apresentacao.Service.git
+git clone https://github.com/desenvspintec/Ioutility.Storage.Service.git
+git clone https://github.com/desenvspintec/Ioutility.Testes.BDD.git
+git clone https://github.com/desenvspintec/Iotuility.Cadastro.Service.git
+git clone https://github.com/desenvspintec/Ioutility.ApiGateway.Service.git
